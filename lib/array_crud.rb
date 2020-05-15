55a54760b138e47763f2a362d92ed440d7a170ce
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-15 08:31:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array=["wow", "I", "am", "so"]
-  array(index_number[2])
+  ["wow", "I", "am", "so"](index_number[2])
 end
 
 def retrieve_first_element_from_array(array)
